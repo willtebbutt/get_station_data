27513bd98d02d14a18f34e43122e873e37615ce7
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2019-05-09 18:39:54 +0100

    Minor alteration

--- plot_locations.py
+++ plot_locations.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Load the data and pull out the unique lats and lons
+# Load the data and pull out the unique lats and lons.
+# Columns are lat, lon, elevation, month_of_year, T
 data = np.genfromtxt('global_station_data.csv', delimiter=',', skip_header=1)
+
 latlon = data[:, 0:2]
 unique_latlon = np.unique(latlon, axis=0)
 lats, lons = unique_latlon[:, 0], unique_latlon[:, 1]
